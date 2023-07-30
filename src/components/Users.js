7359ac97038616373bb0787d7bad7a56caee9f79
towficaziz <tapolly01@gmail.com>
2023-07-30 13:32:20 +0200

    Introducing error boundaris

--- src/components/Users.js
+++ src/components/Users.js
@@ -13,6 +13,13 @@ class Users extends Component {
       more: 'Test',
     };
   }
+
+  componentDidUpdate(){
+    if(this.props.users.length === 0){
+      throw new Error ('No users provided!');
+    }
+  }
+  
   toggleUsersHandler(){
     // this.state.showUsers = false; // NOT!
     this.setState((curState) => {
