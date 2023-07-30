67470afbfdbcd67537a7726565b71538e8bb4d2a
towficaziz <tapolly01@gmail.com>
2023-07-30 12:01:27 +0200

    class based components & context

--- src/components/UserFinder.js
+++ src/components/UserFinder.js
@@ -2,26 +2,33 @@ import { Fragment, useState, useEffect, Component } from "react";
 
 import Users from "./Users";
 import classes from "./UserFinder.module.css";
+import UsersContext from "../store/users-context";
 
-const DUMMY_USERS = [
-  { id: "u1", name: "Max" },
-  { id: "u2", name: "Manuel" },
-  { id: "u3", name: "Julie" },
-];
+// const DUMMY_USERS = [
+//   { id: "u1", name: "Max" },
+//   { id: "u2", name: "Manuel" },
+//   { id: "u3", name: "Julie" },
+// ];
 
 class UserFinder extends Component {
+  static contextType = UsersContext;
+
   constructor() {
     super();
     this.state = {
-      filteredUsers: DUMMY_USERS,
+      filteredUsers: [],
       searchTerm: "",
     };
+  }
+
+  componentDidMount(){
+    this.setState({filteredUsers: this.context.users});
   }
 
   componentDidUpdate(prevProps, prevState) {
     if(prevState.searchTerm !== this.state.searchTerm){
       this.setState({
-        filteredUsers: DUMMY_USERS.filter((user) =>
+        filteredUsers: this.context.users.filter((user) =>
           user.name.includes(this.state.searchTerm)
         ),
       });
@@ -36,6 +43,7 @@ class UserFinder extends Component {
   render() {
     return (
       <Fragment>
+        
         <div className={classes.finder}>
           <input type="search" onChange={this.searchChangeHandler.bind(this)} />
         </div>
